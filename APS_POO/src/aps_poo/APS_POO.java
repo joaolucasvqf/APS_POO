1456937ba25c39469f5f0818f78b7de6f2d3e895
joaolucasvqf <joaofonseca@alunos.utfpr.edu.br>
2018-11-15 12:25:30 -0200

    Agora tá certo essa caralha

--- APS_POO/src/aps_poo/APS_POO.java
+++ APS_POO/src/aps_poo/APS_POO.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package aps_poo;
 
 import javafx.application.Application;
@@ -13,10 +8,6 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-/**
- *
- * @author João Lucas
- */
 public class APS_POO extends Application {
     
     @Override
